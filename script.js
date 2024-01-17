815728416a3437f8400e9484c9d319e61c000fea
Krisjanis Dzenis 221RDB029 <113845787+krisjanisdz@users.noreply.github.com>
2024-01-17 20:49:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");
+    sidebar = body.querySelector(".sidebar");
+    toggle = body.querySelector(".toggle");
+    searchBtn = body.querySelector(".search-box");
+    modeSwitch = body.querySelector(".toggle-switch");
+    modeText = body.querySelector(".mode-text");
+
+    modeSwitch.addEventListener("click", () =>{
+        body.classList.toggle("dark");
+    });
+
+    toggle.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
+    });
