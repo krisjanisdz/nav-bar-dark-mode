eb11c7116dac5ac6dc5aaef9bc2f46bd8ef3b344
Krisjanis Dzenis 221RDB029 <113845787+krisjanisdz@users.noreply.github.com>
2024-01-23 19:17:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ const body = document.querySelector("body");
     searchBtn = body.querySelector(".search-box");
     modeSwitch = body.querySelector(".toggle-switch");
     modeText = body.querySelector(".mode-text");
+    toggleElement = body.querySelector('.sidebar header .toggle');
 
     modeSwitch.addEventListener("click", () =>{
         body.classList.toggle("dark");
+        modeText.text = "Light Mode";
+    });
+
+    searchBtn.addEventListener("click", () =>{
+        const isSidebarClosed = sidebar.classList.contains("close");
+
+        sidebar.classList.remove("close");
+
+        if (isSidebarClosed) {
+        toggleElement.classList.toggle('rotated');
+    }
     });
 
     toggle.addEventListener("click", () =>{
         sidebar.classList.toggle("close");
+    });
+
+    toggle.addEventListener("click", () =>{
+        toggleElement.classList.toggle('rotated');
     });
